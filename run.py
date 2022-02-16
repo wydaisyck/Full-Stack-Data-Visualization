3b40c69cf7a2ecfd0808e19c6b669375b4007233
wydaisyck <78733967+wydaisyck@users.noreply.github.com>
2022-02-16 15:47:58 -0800

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app.utils.connectdb import MongoData
 from app.utils.get_table_result import getDatasInStore
 
 if __name__ == "__main__":
-    dh = MongoData(server="192.168.1.119", port=27017, user='wangyidan', passwd='wangyidan321', db_name="zcs", table_name="factor_layering_return")
+    dh = MongoData(server="", port="", user='wangyidan', passwd='wangyidan321', db_name="zcs", table_name="factor_layering_return")
     pdata = dh.process_data()
     pdata.to_pickle('mongodata.pkl')
     print("数据存储成功！")
